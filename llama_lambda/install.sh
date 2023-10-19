60a0118a98a7d99d0fc58a70845af2e5406c578e
Arun Prabhakar <arunprabhakar.t@gmail.com>
2023-10-19 23:32:41 +0530

    Update install.sh
    updated to The Bloke's Llama 2 13b GGML
    [https://huggingface.co/TheBloke/Llama-2-13B-GGML/blob/main/llama-2-13b.ggmlv3.q4_0.bin]

--- llama_lambda/install.sh
+++ llama_lambda/install.sh
@@ -5,9 +5,9 @@ source .venv/bin/activate
 pip3 install -r requirements.txt
 
 
-read -p "Please provide the direct link to the model you'd like to use (Default https://huggingface.co/vihangd/open_llama_7b_300bt_ggml/resolve/main/ggml-model-q4_0.bin):" MODELURL
+read -p "Please provide the direct link to the model you'd like to use (Default https://huggingface.co/TheBloke/Llama-2-13B-GGML/blob/main/llama-2-13b.ggmlv3.q4_0.bin):" MODELURL
 
-MODELURL=${MODELURL:-https://huggingface.co/vihangd/open_llama_7b_300bt_ggml/resolve/main/ggml-model-q4_0.bin}
+MODELURL=${MODELURL:-https://huggingface.co/TheBloke/Llama-2-13B-GGML/blob/main/llama-2-13b.ggmlv3.q4_0.bin}
 
 echo "Downloading $MODELURL"
 
