8250c0e1de9f76f92bdd76fe02e45116c9e192e6
Arun Prabhakar <arunprabhakar.t@gmail.com>
2023-10-19 23:40:18 +0530

    Update install.sh

--- llama_lambda/install.sh
+++ llama_lambda/install.sh
@@ -5,9 +5,9 @@ source .venv/bin/activate
 pip3 install -r requirements.txt
 
 
-read -p "Please provide the direct link to the model you'd like to use (Default https://huggingface.co/TheBloke/Llama-2-13B-GGML/blob/main/llama-2-13b.ggmlv3.q4_0.bin):" MODELURL
+read -p "Please provide the direct link to the model you'd like to use (Default https://huggingface.co/TheBloke/Llama-2-13B-GGML/resolve/main/llama-2-13b.ggmlv3.q2_K.bin):" MODELURL
 
-MODELURL=${MODELURL:-https://huggingface.co/TheBloke/Llama-2-13B-GGML/blob/main/llama-2-13b.ggmlv3.q4_0.bin}
+MODELURL=${MODELURL:-https://huggingface.co/TheBloke/Llama-2-13B-GGML/resolve/main/llama-2-13b.ggmlv3.q2_K.bin}
 
 echo "Downloading $MODELURL"
 
